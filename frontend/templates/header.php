5a68004ad572d75e1dc2b8f670872f448d8d2f7e
Naty20 <165080675+2024Nat@users.noreply.github.com>
2025-09-30 07:58:04 -0300

    fix: Corrige o caminho dos arquivos CSS no header

--- frontend/templates/header.php
+++ frontend/templates/header.php
@@ -9,10 +9,10 @@
     
     <title><?php echo isset($titulo_pagina) ? $titulo_pagina : 'SmileUp Odontologia'; ?></title>
     
-    <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="/frontend/css/home.css">
     
     <?php if (isset($is_dashboard) && $is_dashboard): ?>
-        <link rel="stylesheet" href="css/dashboard.css">
+        <link rel="stylesheet" href="/frontend/css/dashboard.css">
     <?php endif; ?>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -34,11 +34,11 @@
                     // --- MENU DO DENTISTA ---
                     if ($tipo_usuario === 'dentista'):
                 ?>
-                        <li><a href="dashboard-dentista.php#agenda-do-dia" class="nav-link">Minha Agenda</a></li>
-                        <li><a href="dashboard-dentista.php#meus-horarios" class="nav-link">Gerenciar Horários</a></li>
-                        <li><a href="meus-pacientes.php" class="nav-link">Meus Pacientes</a></li>
-                        <li><a href="dashboard-dentista.php#estoque" class="nav-link">Estoque </a></li>
-                        <li><a href="editar-perfil-dentista.php" class="nav-link">Editar Perfil</a></li>
+                        <li><a href="/dashboard-dentista.php#agenda-do-dia" class="nav-link">Minha Agenda</a></li>
+                        <li><a href="/dashboard-dentista.php#meus-horarios" class="nav-link">Gerenciar Horários</a></li>
+                        <li><a href="/meus-pacientes.php" class="nav-link">Meus Pacientes</a></li>
+                        <li><a href="/dashboard-dentista.php#estoque" class="nav-link">Estoque </a></li>
+                        <li><a href="/editar-perfil-dentista.php" class="nav-link">Editar Perfil</a></li>
                         <li><a href="../backend/logout.php" class="nav-link btn-secondary" style="padding: 0.5rem 1rem; border-radius: 9999px;">Sair</a></li>
 
                 <?php
@@ -48,6 +48,7 @@
                         <li><a href="dashboard-paciente.php#consultas" class="nav-link">Minhas Consultas</a></li>
                         <li><a href="agendar_consulta.php" class="nav-link">Agendar</a></li>
                         <li><a href="dashboard-paciente.php#laudos" class="nav-link">Meus Laudos</a></li>
+                        <li><a href="perfil-paciente.php" class="nav-link">Ver Perfil</a></li>
                         <li><a href=" " class="nav-link">Olá, <?php echo htmlspecialchars(explode(' ', $nome_usuario)[0]); ?>!</a></li>
                         <li><a href="../backend/logout.php" class="nav-link btn-secondary" style="padding: 0.5rem 1rem; border-radius: 9999px;">Sair</a></li>
 
